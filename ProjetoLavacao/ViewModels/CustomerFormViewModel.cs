0adc1e619ff7fc69afa9376a0daa8d7d08daef19
Ricardo Alvize <Ricardo Alvize@DESKTOP-0T85HB8>
2017-10-31 21:27:10 -0200

    Atualizado

---
+++ ProjetoLavacao/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoLavacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLavacao.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Customer != null && Customer.Id != 0)
+                    return "Editar Cliente";
+
+                return "Novo Cliente";
+            }
+        }
+    
+}
+}
---
+++ ProjetoLavacao/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoLavacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLavacao.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
