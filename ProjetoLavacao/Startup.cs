f334e9f7f99afd35ac1237e49e9d802b6be814bb
Ricardo Alvize <Ricardo Alvize@DESKTOP-0T85HB8>
2017-10-31 19:34:00 -0200

    Add project files.

---
+++ ProjetoLavacao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoLavacao.Startup))]
+namespace ProjetoLavacao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
