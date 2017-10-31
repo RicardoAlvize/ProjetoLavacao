02df6c8b4478611cfb8e26bde703d7559323e105
Ricardo Alvize <Ricardo Alvize@DESKTOP-0T85HB8>
2017-10-31 21:03:54 -0200

    Atualizado

---
+++ ProjetoLavacao/Controllers/NovoClienteController.cs
@@ -0,0 +1,36 @@
+﻿using ProjetoLavacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoLavacao.Controllers
+{
+    public class NovoClienteController : Controller
+    {
+        public List<NovoCliente> Customers = new List<NovoCliente>
+        {
+
+            new NovoCliente {Id = 1 , Nome = "Ricardo", Sobrenome = "Alvize", Cpf = 92119535, Endereco = "Rua Carlos Zemke",Telefone = 32736161 }
+
+        };
+
+        // GET: Cadastros
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var NovoCliente = Customers.SingleOrDefault(c => c.Id == id);
+
+
+            if (NovoCliente == null)
+                return HttpNotFound();
+
+            return View(NovoCliente);
+        }
+    }
+}
---
+++ ProjetoLavacao/Models/NovoCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLavacao.Models
+{
+    public class NovoCliente
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Sobrenome { get; set; }
+
+        public int Cpf { get; set; }
+
+        public String Endereco { get; set; }
+
+        public int Telefone { get; set; }
+    }
+}
